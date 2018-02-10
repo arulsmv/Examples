eed6023677d2cbed0e1e8e6a4ec74677812d5694
Arul Siva Murugan V <arulsmv@gmail.com>
2018-02-10 08:17:13 +0530

    Simple Helloword program to test diff.

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,18 @@
+/** 
+ * This is the first java file used for testing.
+ * Git Repo.
+ */
+
+package com.example;
+
+import java.io.*;
+import java.util.*;
+
+public class HelloWorld {
+  public HelloWorld() {
+  }
+  public static void main(String[] args) {
+     HelloWorld hw = new HelloWorld();
+     System.out.println("Hello");
+  }
+}
