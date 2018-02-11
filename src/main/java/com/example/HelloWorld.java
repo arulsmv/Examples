c613ec63ba07f7528ee3c3e25c9c1d9f3060f7d5
Arul Siva Murugan V <arulsmv@gmail.com>
2018-02-12 00:43:46 +0530

    making as a function

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -9,10 +9,18 @@ import java.io.*;
 import java.util.*;
 
 public class HelloWorld {
+  private String name;
   public HelloWorld(String name) {
+     this.name = name;
+  }
+
+  public void wish() {
      System.out.println("Hello "+ name);
   }
+
   public static void main(String[] args) {
      HelloWorld hw = new HelloWorld(args[0]);
+     hw.wish();
   }
+
 }
